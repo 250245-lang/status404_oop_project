d1e85bf58d040babbd9e1624753d1dc9f6880a85
250254-alt <250254-alt@users.noreply.github.com>
2026-03-15 17:30:00 +0500

    Implemented base Person class and Location model

---
+++ src/main/java/parkinglot/models/Location.java
@@ -0,0 +1,3 @@
+package parkinglot.models;
+
+public record Location(String streetAddress, String city, String state, String zipcode, String country){}
---
+++ src/main/java/parkinglot/users/Person.java
@@ -0,0 +1,10 @@
+package parkinglot.users;
+
+import parkinglot.models.Location;
+
+public record Person(
+        String name,
+        Location address,
+        String email,
+        String phone
+) {}
