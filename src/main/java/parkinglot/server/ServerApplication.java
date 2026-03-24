6cdc9cfc25160ff76f7d4960d445d7aeb378e956
250245-lang <250245-lang@users.noreply.github.com>
2026-03-24 10:30:00 +0500

    Implemented initial Admin creation in ServerApplication

--- src/main/java/parkinglot/server/ServerApplication.java
+++ src/main/java/parkinglot/server/ServerApplication.java
@@ -1,9 +1,15 @@
 package parkinglot.server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import parkinglot.models.Location;
+import parkinglot.users.Admin;
+import parkinglot.users.Person;
+import parkinglot.server.repository.AccountRepository;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"parkinglot"})
@@ -12,6 +18,21 @@ public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
-        System.out.println("Parking Lot Server started with H2/JPA!");
+    }
+
+    @Bean
+    public CommandLineRunner initData(AccountRepository accountRepo) {
+        return args -> {
+            if (accountRepo.count() == 0) {
+                Person adminInfo = new Person("System Admin",
+                        new Location("Movarounnahra street 1", "Tashkent", "TSH", "100", "UZB"),
+                        "admin@newuu.uz", "998901234567");
+
+                Admin admin = new Admin("admin", "admin", adminInfo);
+                accountRepo.save(admin);
+
+                System.out.println("Default Admin account created: admin/admin");
+            }
+        };
     }
 }
