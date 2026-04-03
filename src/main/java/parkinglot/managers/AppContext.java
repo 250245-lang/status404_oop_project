7cd681789c6914a014795e355167392e0a8633c2
Beklir <Beklir@users.noreply.github.com>
2026-04-03 14:00:00 +0500

    AppContext: Global storage for JWT and User state

--- src/main/java/parkinglot/managers/AppContext.java
+++ src/main/java/parkinglot/managers/AppContext.java
@@ -1,12 +1,17 @@
 package parkinglot.managers;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.stage.Stage;
+import parkinglot.models.ParkingLot;
 import parkinglot.users.Account;
 
 public class AppContext {
     public final Stage stage;
     public final APIManager apiManager = new APIManager();
     public Account account; 
+    
+    private final ObjectProperty<ParkingLot> parkingLot = new SimpleObjectProperty<>();
 
     public AppContext(Stage stage) {
         this.stage = stage;
@@ -20,6 +25,10 @@ public class AppContext {
         setAccount(null);
         apiManager.clearToken();
     }
+
+    public ObjectProperty<ParkingLot> parkingLotProperty() { return parkingLot; }
+    public ParkingLot getParkingLot() { return parkingLot.get(); }
+    public void setParkingLot(ParkingLot lot) { this.parkingLot.set(lot); }
 
     public void resetToView(javafx.scene.Parent view, String title, double width, double height, boolean resizable) {
         stage.setTitle(title);
